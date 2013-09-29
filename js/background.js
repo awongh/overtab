434e31785de225a04736c39d98cee5c2f0fb9e93
Peter Gao <pgao@quantcast.com>
2013-09-29 15:37:40 -0700

    Work in progress refactor, using sanitizeTab and updateTab to handle tab browsing (changing URLs, etc)

--- js/background.js
+++ js/background.js
@@ -22,30 +22,40 @@ document.addEventListener("DOMContentLoaded", function() {
 function addScreencap(tab) {
     if (typeof tabListIndex[tab.id] !== "undefined") {
         tabList[tabListIndex[tab.id]].screencap = tab.screencap;
+        updateTabLists();
     }
 }
 
-function addTab(tab) {
+function addTab(tab, callback) {
 
     if (typeof tab.tabId !== "undefined") { // The user passed in a tabInfo object, not a tab object
         // Is this tab already in our index?
         if (typeof tabListIndex[tab.tabId] === "undefined") {
             // We need to go and fetch the actual tab object now
             chrome.tabs.get(tab.tabId, function(tabObject) {
-
-                // Add the tab object and index lookup into their respective arrays
-                tabList.push(tabObject);
-                tabListIndex[tabObject.id] = tabList.length - 1;
-                updateTabLists();
+                if (tab.url.match(/^http.*:\/\//) || tab.title === "New Tab") {
+                    // Add the tab object and index lookup into their respective arrays
+                    tabList.push(tabObject);
+                    tabListIndex[tabObject.id] = tabList.length - 1;
+                    updateTabLists();
+                    
+                    if (typeof callback !== "undefined") {
+                        callback(tabObject);
+                    }
+                }
             });
         }
-    } else if (typeof tab.id !== "undefined") { // The object is (probably) a tab object, yay!
+    } else if (typeof tab.id !== "undefined" && (tab.url.match(/^http.*:\/\//) || tab.title === "New Tab")) { // The object is (probably) a tab object, yay!
         // Is this tab already in our index?
         if (typeof tabListIndex[tab.id] === "undefined") {
             // Add the tab index and object into their respective arrays
             tabList.push(tab);
             tabListIndex[tab.id] = tabList.length - 1;
             updateTabLists();
+
+            if (typeof callback !== "undefined") {
+                callback(tab);
+            }
         }
     }
 }
@@ -85,15 +95,49 @@ function removeTab(tab) {
     updateTabLists();
 }
 
+function sanitizeTab(tab, callback) {
+    var tabId = null,
+        tabPosition = null;
+
+    if (typeof tab.tabId !== "undefined") { // The user passed in a tabInfo object, not a tab object
+        // Is this tab still in our index?
+        if (typeof tabListIndex[tab.tabId] !== "undefined") {
+            tabId = tab.tabId;
+        } else {
+            return false;
+        }
+    } else if (typeof tab.id !== "undefined") { // The object is (probably) a tab object, yay!
+        // Is this tab still in our index?
+        if (typeof tabListIndex[tab.id] !== "undefined") {
+            tabId = tab.id;
+        } else {
+            return false;
+        }
+    } else if (typeof tab === "number") { // The variable is the tab ID as an integer
+        // Is this tab still in our index?
+        if (typeof tabListIndex[tab] !== "undefined") {
+            tabId = tab;
+        } else {
+            return false;
+        }
+    }}
+
+// Updates an existing tab entry, and calls the given callback afterwards (optional)
+function updateTab(tab, callback) {
+    sanitizeTab(tab, function(tab) {
+        if (typeof callback !== "undefined") {
+            callback(tab);
+        }
+    });
+}
+
 // This function re-indexes the tabListIndex after a tab is removed from the tabList array
 // so that the reverse index in tabListIndex is still pointing to the right Array element
 function reIndex(tabPosition) {
     for (var tabIndex in tabListIndex) {
         if (tabListIndex.hasOwnProperty(tabIndex)) {
-            if (tabListIndex[tabIndex]) {
-                if (tabListIndex[tabIndex] >= tabPosition) {
-                    tabListIndex[tabIndex] -= 1;
-                }
+            if (tabListIndex[tabIndex] >= tabPosition) {
+                tabListIndex[tabIndex] -= 1;
             }
         }
     }
@@ -117,30 +161,47 @@ function updateTabLists() {
 
 // This will execute whenever a tab has completed "loading"
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
+    updateTab(tab);
+    console.log("Being updated: ", changeInfo);
     if (changeInfo.status === "complete") {
-        captureScreen(tab);
+        chrome.tabs.query({ currentWindow: true, windowId: tab.windowId, active: true, status: "complete" }, function(tabs) {
+            console.log(tabs);
+            console.log(tab);
+            if (tabs.length > 0 && tabs[0].id == tab.id) {
+                captureScreen(tab);
+            }
+        });
     }
+});
+
+chrome.tabs.onCreated.addListener(function(tab) {
+    addTab(tab);
 });
 
 chrome.tabs.onActivated.addListener(function(tabInfo) {
     var tabId = tabInfo.tabId;
 
     chrome.tabs.get(tabId, function(tab) {
-        captureScreen(tab);
+        addTab(tab, function(tab) {
+            chrome.tabs.query({ currentWindow: true, windowId: tab.windowId, active: true, status: "complete" }, function(tabs) {
+                if (tabs.length > 0 && tabs[0].id == tab.id) {
+                    captureScreen(tab);
+                }
+            });
+        });
     });
 });
 
 function captureScreen(tab) {
 
     // Check to see if this is a Chrome internal page. If so, don't capture it
-    if (tab.url.match(/^http.*:\/\//) && !screencapExists(tab)) {
+    if ((tab.url.match(/^http.*:\/\//) || tab.title === "New Tab") && !screencapExists(tab)) {
         chrome.tabs.captureVisibleTab(tab.windowId, {format: "png"}, function(imgBlob) {
             tab["screencap"] = imgBlob;
             tab["timestamp"] = Date.getTime();
             if (tabExists(tab)) {
                 addScreencap(tab);
-            } else {
-                addTab(tab);
             }
 
             // Work in progress code for shrinking the image
