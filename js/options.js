313fb3eae5de9decbeaf90b8e3df9f312f354020
awongh <akira@awongh.com>
2014-01-26 17:02:32 -0800

    dummy options js file

---
+++ js/options.js
@@ -0,0 +1,34 @@
+// Saves options to localStorage.
+function save_options() {
+  var select = document.getElementById("color");
+
+  var input = document.getElementById("open-as-window");
+
+  //get it
+  var windowOption = input.value;
+
+  localStorage["windowOption"] = windowOption;
+
+  // Update status to let user know options were saved.
+  var status = document.getElementById("status");
+  status.innerHTML = "Options Saved.";
+  setTimeout(function() {
+    status.innerHTML = "";
+  }, 750);
+}
+
+function restore_options() {
+
+  var windowOption = localStorage["windowOption"];
+
+  if (!windowOption) {
+    return;
+  }
+
+  var input = document.getElementById("window-as-option");
+
+  input.value = winodwOption;
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.querySelector('#save').addEventListener('click', save_options);
