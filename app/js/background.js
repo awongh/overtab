44676ca353342c5f04d8c1c1624b5ab9c989ed4b
awongh <akira@awongh.com>
2014-02-13 18:50:14 -0800

    major changes/refactor for event based local storage background.js only

--- app/js/background.js
+++ app/js/background.js
@@ -1,6 +1,18 @@
 "use strict";
 
+//VARIABLES
+
+  var OVERTAB_TAB_ID = null,
+      OVERTAB_WINDOW_ID = null;
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////        CONVINIENCE CLASSES             ////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
 Array.prototype.remove = function(from) {
+    return this.splice(from, 1);
 };
 
 Array.prototype.add = function(from, item) {
@@ -8,245 +20,56 @@ Array.prototype.add = function(from, item) {
       throw "Trying to add a null variable to array. -oops";
     }
 
-    return this.splice(from, 1, item);
+    return this.splice(from, 0, item);
 };
 
-var tabList = [],
-    tabListIndex = {}, // For checking to see if a tab already exists, and for reverse lookup of the tab in tabList array
-    tabOpened = false,
-    canvas = null,
-    image = null,
-    date = new Date(),
-    overTabId = null,
-
-    overTabWindowId = null;
-
-document.addEventListener("DOMContentLoaded", function() {
-    canvas = document.querySelector('canvas');
-    image = document.querySelector('canvas');
-});
-
-// This function will accept tab object, tab id, tab info, and a callback function.
-// It will always pass into the callback function a tab object
-function sanitizeTab(dirtyTab, callback) {
-    var dirtyTabId = null;
-
-    if (typeof dirtyTab === "undefined") {
-        return false;
-    } else {
-        if (typeof dirtyTab.tabId !== "undefined" || typeof dirtyTab === "number") {
-            dirtyTabId = dirtyTab.tabId || dirtyTab;
-        }
-    }
-
-    // The input was a tab ID, so fetch the actual tab object
-    if (typeof dirtyTabId !== "undefined" && dirtyTabId !== null) {
-        getTab(dirtyTabId, function(tabObject) {
-            if (typeof tabObject !== "undefined" && (tabObject.url.match(/^http.*:\/\//) || dirtyTab.title === "New Tab" )) {
-                callback(tabObject);
-            }
-        });
-    // The input was a tab object, run the callback directly on it :)
-    } else if (typeof dirtyTab.id !== "undefined" && (dirtyTab.url.match(/^http.*:\/\//) || dirtyTab.title === "New Tab" )) {
-        callback(dirtyTab);
-    }
+var Parser = function( href ){
+  this.constructor;
+  this.parser = document.createElement('a');
 }
 
-function sendSingleTab( tab ) {
-    sanitizeTab(tab, function(tab) {
-        console.log( tab );
-        sendMessage(null, { message:"sendSingleTab", tab: tab });
-    });
+Parser.prototype = {
+  href : function( href ){
+    this.parser.href = href;
+    return this;
+  },
+
+  protocol : function(){
+    return this.parser.protocol;
+  },
+
+  hostname : function(){
+    return this.parser.hostname;
+  },
+
+  port : function(){
+    return this.parser.port;
+  },
+
+  pathname : function(){
+    return this.parser.pathname;
+  },
+
+  search : function(){
+    return this.parser.search;
+  },
+
+  hash : function(){
+    return this.parser.hash;
+  },
+
+  host : function(){
+    return this.parser.host;
+  }
 }
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////      END CONVINIENCE CLASSES           ////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
-function addTab(tab, callback) {
-
-    if (typeof tab.tabId !== "undefined") { // The user passed in a tabInfo object, not a tab object
-        // Is this tab already in our index?
-        if (typeof tabListIndex[tab.tabId] === "undefined") {
-            // We need to go and fetch the actual tab object now
-            getTab(tab.tabId, function(tabObject) {
-                if (tab.url.match(/^http.*:\/\//) || tab.title === "New Tab") {
-                    // Add the tab object and index lookup into their respective arrays
-                    tabList.push(tabObject);
-                    tabListIndex[tabObject.id] = tabList.length - 1;
-                    sendSingleTab(tabObject);
-
-                    if (typeof callback !== "undefined") {
-                        callback(tabObject);
-                    }
-                }
-            });
-        }
-    } else if (typeof tab.id !== "undefined" && (tab.url.match(/^http.*:\/\//) || tab.title === "New Tab")) { // The object is (probably) a tab object, yay!
-        // Is this tab already in our index?
-        if (typeof tabListIndex[tab.id] === "undefined") {
-            // Add the tab index and object into their respective arrays
-            tabList.push(tab);
-            tabListIndex[tab.id] = tabList.length - 1;
-            sendSingleTab(tab);
-
-            if (typeof callback !== "undefined") {
-                callback(tab);
-            }
-        }
-    }
-}
-
-// This function re-indexes the tabListIndex after a tab is removed from the tabList array
-// so that the reverse index in tabListIndex is still pointing to the right Array element
-function reIndex(tabPosition) {
-    for (var tabIndex in tabListIndex) {
-        if (tabListIndex.hasOwnProperty(tabIndex)) {
-            if (tabListIndex[tabIndex] >= tabPosition) {
-                tabListIndex[tabIndex] -= 1;
-            }
-        }
-    }
-}
-
-function sendRemoveTab( tabId ) {
-    sendMessage(null, { message:"sendRemoveTab", tabId: tabId });
-}
-
-function removeTab(tab) {
-    var tabId = null,
-        tabPosition = null;
-
-    if (typeof tab.tabId !== "undefined") { // The user passed in a tabInfo object, not a tab object
-        // Is this tab still in our index?
-        if (typeof tabListIndex[tab.tabId] !== "undefined") {
-            tabId = tab.tabId;
-        } else {
-            return false;
-        }
-    } else if (typeof tab.id !== "undefined") { // The object is (probably) a tab object, yay!
-        // Is this tab still in our index?
-        if (typeof tabListIndex[tab.id] !== "undefined") {
-            tabId = tab.id;
-        } else {
-            return false;
-        }
-    } else if (typeof tab === "number") { // The variable is the tab ID as an integer
-        // Is this tab still in our index?
-        if (typeof tabListIndex[tab] !== "undefined") {
-            tabId = tab;
-        } else {
-            return false;
-        }
-    }
-
-    tabPosition = tabListIndex[tabId];
-    tabList.remove(tabPosition);
-    delete tabListIndex[tabId];
-
-    reIndex(tabPosition);
-    sendRemoveTab(tabId);
-}
-
-// Updates an existing tab entry, and calls the given callback afterwards (optional)
-// If the tab doesn't exist yet, invoke the addTab function instead
-function updateTab(tab, callback) {
-
-    sanitizeTab(tab, function(tab) {
-        var updated = false;
-
-        if (tabList[tabListIndex[tab.id]]) {
-            if (tabList[tabListIndex[tab.id]].url !== tab.url) {
-                tabList[tabListIndex[tab.id]].url = tab.url;
-                delete tabList[tabListIndex[tab.id]].screencap;
-                delete tabList[tabListIndex[tab.id]].timestamp;
-                updated = true;
-            }
-
-            if (tabList[tabListIndex[tab.id]].status !== tab.status) {
-                tabList[tabListIndex[tab.id]].status = tab.status;
-                updated = true;
-            }
-
-            if (tabList[tabListIndex[tab.id]].title !== tab.title) {
-                tabList[tabListIndex[tab.id]].title = tab.title;
-                updated = true;
-            }
-
-            //**Add new properties to track for updating here**//
-
-            if (updated) {
-                sendSingleTab(tab);
-            }
-
-            if (typeof callback !== "undefined") {
-                callback(tab);
-            }
-        } else {
-            //alert("wut");
-            console.log( "other tabc" );
-            itabCreated(tab, callback(tab));
-        }
-    });
-}
-
-// Expect an actual tab object
-function tabExists(tab) {
-    return typeof tabListIndex[tab.id] !== "undefined" ? true : false;
-}
-
-// Expect an actual tab object
-function screencapExists(tab) {
-    if (tabExists(tab)) {
-        return typeof tabList[tabListIndex[tab.id]].screencap !== "undefined" ? true : false;
-    } else {
-        return false;
-    }
-}
-
-function sendTabLists() {
-    sendMessage(null, {message: "sendTabLists", tabList: tabList, tabListIndex: tabListIndex}, function() {});
-}
-
-function captureScreen(tab) {
-
-    sanitizeTab(tab, function(tab) {
-
-        screenCap(tab.windowId, {format: "png"}, function(imgBlob) {
-            var canvas = document.getElementById('canvas'),
-                canvasContext = canvas.getContext('2d'),
-                img = document.getElementById('img'),
-                ratio = tab.height / tab.width,
-                quarter;
-
-            img.onload = function() {
-                canvasContext.clearRect( 0, 0, canvas.width, canvas.height);
-                if (ratio > 1) { // Screenshot is taller than it is wide
-                    canvasContext.drawImage(this, 0, 0, canvas.width * window.devicePixelRatio, canvas.height * ratio * window.devicePixelRatio);
-                } else {
-                    canvasContext.drawImage(this, 0, 0, canvas.width * tab.width / tab.height * window.devicePixelRatio, canvas.height * window.devicePixelRatio);
-                }
-
-                tab.screencap = canvas.toDataURL();
-                tab.timestampSinceCapture = date.getTime();
-                if (tabExists(tab)) {
-                    tabList[tabListIndex[tab.id]] = tab;
-                    sendSingleTab(tabList[tabListIndex[tab.id]]);
-
-                    if (!tab.favIconUrl || tab.favIconUrl === ''){
-
-                        setTimeout(function() {
-                            /// set favicon wherever it needs to be set here
-                            console.log('delay tabId', tab.id);
-                            getTab(tab.id, function(tab){
-
-                              sendMessage(null, {message: "faviconTab", tab: tab}, function() {});
-                            });
-
-                        }, 4000);
-                    }
-                }
-            };
-
-            img.src = imgBlob; // Set the image to the dataUrl and invoke the onload function
-        });
-    });
+function tabEvent( id, message ){
+  sendMessage(null, {message: message, id: id});
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -255,120 +78,107 @@ function captureScreen(tab) {
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
-
-var itabCreated = function( tab ){
-
-  console.log( "itabcreated", tab );
-
-  if (typeof tab.tabId !== "undefined") { // The user passed in a tabInfo object, not a tab object
-      // Is this tab already in our index?
-      if (typeof tabListIndex[tab.tabId] === "undefined") {
-          // We need to go and fetch the actual tab object now
-          getTab( tab.tabId, function(tabObject) {
-              if (tab.url.match(/^http.*:\/\//) || tab.title === "New Tab") {
-                  // Add the tab object and index lookup into their respective arrays
-                  tabList.push(tabObject);
-                  tabListIndex[tabObject.id] = tabList.length - 1;
-                  sendSingleTab(tabObject);
-
-                  if (typeof callback !== "undefined") {
-                      callback(tabObject);
-                  }
-              }
-          });
-      }
-  } else if (typeof tab.id !== "undefined" && (tab.url.match(/^http.*:\/\//) || tab.title === "New Tab")) { // The object is (probably) a tab object, yay!
-      // Is this tab already in our index?
-      if (typeof tabListIndex[tab.id] === "undefined") {
-          // Add the tab index and object into their respective arrays
-          tabList.push(tab);
-          tabListIndex[tab.id] = tabList.length - 1;
-          sendSingleTab(tab);
-
-          if (typeof callback !== "undefined") {
-              callback(tab);
-          }
-      }
-  }
-
-};
-
 var tabCreated = function( tab ){
 
-  console.log( "tabcreated", tab );
-  if (typeof tab.tabId !== "undefined") { // The user passed in a tabInfo object, not a tab object
-      // Is this tab already in our index?
-      if (typeof tabListIndex[tab.tabId] === "undefined") {
-          // We need to go and fetch the actual tab object now
-          getTab( tab.tabId, function(tabObject) {
-              if (tab.url.match(/^http.*:\/\//) || tab.title === "New Tab") {
-                  // Add the tab object and index lookup into their respective arrays
-                  tabList.push(tabObject);
-                  tabListIndex[tabObject.id] = tabList.length - 1;
-                  sendSingleTab(tabObject);
+  console.log( "tabcreated - undefined", tab.id );
+  console.log( "tabcreated - url", tab.url );
+  console.log( "tabcreated - title", tab.title );
 
-                  if (typeof callback !== "undefined") {
-                      callback(tabObject);
-                  }
-              }
-          });
-      }
-  } else if (typeof tab.id !== "undefined" && (tab.url.match(/^http.*:\/\//) || tab.title === "New Tab")) { // The object is (probably) a tab object, yay!
-      // Is this tab already in our index?
-      if (typeof tabListIndex[tab.id] === "undefined") {
-          // Add the tab index and object into their respective arrays
-          tabList.push(tab);
-          tabListIndex[tab.id] = tabList.length - 1;
-          sendSingleTab(tab);
+  var parser = new Parser();
 
-          if (typeof callback !== "undefined") {
-              callback(tab);
-          }
+  var tabProtocol = parser.href(tab.url).protocol();
+  var hostName = parser.href(tab.url).hostname();
+
+  //what conditions do we want to accept add adding a tab?
+  if (typeof tab.id !== "undefined"
+    && ( (tabProtocol === "http:" || tabProtocol === "https:" || hostName === "newtab" ) ) ) {
+
+      var id = tab.id;
+      if ( !localStorage.getItem( id ) ) {
+
+          //ya, we are storing nothing in here
+          localStorage.setItem( id, "no-screencap");
+
+          //send it off to the angular app
+          tabEvent( id, "created" );
       }
+  }else{
+    //this might be the overtab tab, or options tab or soemthing
+    console.log( "ERROR: something happened on create:", tab );
+    console.log( "http:",tabProtocol === "http:");
+    console.log( "https:",tabProtocol === "https:");
+    console.log( "hostname", hostName, hostName === "newtab" );
   }
-
 };
 
 var tabUpdated = function( tabId, changeInfo, tab ){
-    console.log( "tabupdated", tabId, changeInfo, tab );
+    console.log( "tabupdated", changeInfo.status );
 
-    updateTab(tab, function() {
-        if (changeInfo.status === "complete") {
-            tabQuery({ currentWindow: true, windowId: tab.windowId, active: true, status: "complete" }, function(tabs) {
-                if (tabs.length > 0 && tabs[0].id == tab.id) {
-                    captureScreen(tab);
-                }
-            });
-        }
-    });
+    if( !localStorage.getItem( tabId ) ){
+      console.log( "ERROR: updated but not in lc:", tabId, changeInfo, tab );
+      return;
+    }
+
+    tabEvent( tabId, "updated" );
 };
 
-var tabActivated = function( tabInfo ){
-   console.log( "tabactiviated", tabInfo );
-   var tabId = tabInfo.tabId;
-
-    sanitizeTab(tabId, function(tab) {
-
-        if (!screencapExists(tab)) {
-            updateTab(tab, function(tab) {
-
-                tabQuery({ currentWindow: true, windowId: tab.windowId, active: true, status: "complete" }, function(tabs) {
-                    if (tabs.length > 0 && tabs[0].id == tab.id) {
-                        captureScreen(tab);
-                    }
-                });
-            });
-        }
+function getFavicon(id){
+  setTimeout(function() {
+    /// set favicon wherever it needs to be set here
+    getTab(id, function(tab){
+      if( tab.favIconUrl ){
+        tabEvent( id, "favicon" );
+      }
     });
+
+  }, 4000);
+}
+
+var tabActivated = function( tabInfo ){
+  console.log( "tabactiviated", tabInfo );
+  var tabId = tabInfo.tabId;
+
+  if( !localStorage.getItem( tabId ) ){
+    console.log( "ERROR: activated but not in lc:", tabInfo);
+    return;
+  }
+
+  tabEvent( tabId, "activated" );
+
+  if( localStorage.getItem( tabId ) !== "no-screencap" ){
+    //we already have a screencap for this
+    console.log( "already has a screenshot" );
+  }
+
+  //needs to be "active" and "complete" to screenshot
+  var activeCompleteQuery = {
+    currentWindow: true,
+    windowId: tabInfo.windowId,
+    active: true,
+    status: "complete"
+  };
+
+  tabQuery(activeCompleteQuery, function(result) {
+    if (result.id == tabId) {
+      screenCap(tabInfo.windowId, {format: "png"}, function( blob ){
+        localStorage.setItem( tabId, blob );
+        tabEvent( tabId, "screencap" );
+      });
+
+      //go off to get a favicon
+      getFavicon(tabId);
+    }
+  });
 };
 
 var tabRemoved = function( tabId, removeInfo ){
-  removeTab(tabId);
-  if (tabId === overTabId) {
-    console.log("Closed");
-    tabOpened = false;
-    overTabId = null;
-    overTabWindowId = null;
+
+  localStorage.removeItem(tabId);
+
+  if (tabId === OVERTAB_TAB_ID) {
+    //console.log("Closed");
+    OVERTAB_TAB_ID = null;
+    OVERTAB_WINDOW_ID = null;
   }
 };
 
@@ -380,10 +190,9 @@ var onMessage = function( request, sender, sendResponse ){
 
 var browserActionClick = function( ){
 
-  if ( !tabOpened ) {
+  if ( OVERTAB_TAB_ID === null ) {
     // Prevents mashing the button and opening duplicate Overtab tabs
-    tabOpened = true;
-    var func = localStorage['overTabFunc'];
+    var func = localStorage.getItem( overTabFunc );
 
     if( !func ){
       //default behavior
@@ -400,18 +209,22 @@ var browserActionClick = function( ){
 
         //do we want any checks here?
 
-        overTabId = tab.id;
-        overTabWindowId = tab.windowId;
+        OVERTAB_TAB_ID = tab.id;
+        OVERTAB_WINDOW_ID = tab.windowId;
+
+        localStorage.setItem( "OVERTAB_TAB_ID", OVERTAB_TAB_ID );
+        localStorage.setItem( "OVERTAB_WINDOW_ID", OVERTAB_WINDOW_ID );
     });
 
   }else {
-    tabFocus( overTabId, overTabWindowId );
+    tabFocus( OVERTAB_TAB_ID, OVERTAB_WINDOW_ID );
   }
 };
 
 var tabReplaced = function( newTabId, oldTabId ){
 
   //replace the old tab with the new one
+  console.log( "ERROR: a tab was replaced" );
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -453,9 +266,15 @@ var sendMessage = function( tabId, message, callback ){
   return chrome.runtime.sendMessage( tabId, message, callback );
 };
 
-//query for a tab
+//query for a single tab
 var tabQuery = function( queryInfo, callback ){
-  return chrome.tabs.query( queryInfo, callback );
+  return chrome.tabs.query( queryInfo, function( tabs ){
+    if (tabs.length > 0 && tabs[0].id) {
+        callback(tabs[0]);
+    }else{
+      console.log( "WARNING: your tab query failed", queryInfo, tabs );
+    }
+  });
 };
 
 //get a tab by id
